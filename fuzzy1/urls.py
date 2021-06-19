be1deda3329962bb215120f2d68ddb944ce60aa0
Suraj Raju Poojari <70881422+surajraju20@users.noreply.github.com>
2021-06-20 02:07:33 +0530

    Update urls.py

--- fuzzy1/urls.py
+++ fuzzy1/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('invoice.urls')),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
