1fb1b6d6a67f5a501f9dd565b65ba02c646b2f08
Suraj Raju Poojari <70881422+surajraju20@users.noreply.github.com>
2021-06-20 02:30:12 +0530

    Update urls.py

--- fuzzy1/urls.py
+++ fuzzy1/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from hypothesis import settings
+from fuzzy1 import settings
 
 urlpatterns = [
     path('', include('invoice.urls')),
     path('admin/', admin.site.urls),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
